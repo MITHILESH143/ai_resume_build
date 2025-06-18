531c640b6a33854258fc7920b4a61e72cfb98591
yash-251198 <shetyeyash444@gmail.com>
2025-06-19 03:02:57 +0530

    started building the first form for general info

--- src/app/(main)/editor/forms/GeneralInfoFrom.tsx
+++ src/app/(main)/editor/forms/GeneralInfoFrom.tsx
@@ -1,5 +1,52 @@
+import { generalInfoSchema, GeneralInfoValue } from "@/lib/validation";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
 const GeneralInfoFrom = () => {
-  return <div></div>;
+  const form = useForm<GeneralInfoValue>({
+    resolver: zodResolver(generalInfoSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+    },
+  });
+
+  return (
+    <div className="mx-auto max-w-xl space-y-6">
+      <div className="space-y-1.5 text-center">
+        <h2 className="text-2xl font-semibold">General Info</h2>
+        <p className="text-muted-foreground text-sm">
+          This will not appear on your resume
+        </p>
+      </div>
+      <Form {...form}>
+        <form action="" className="space-y-3">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Project Name</FormLabel>
+                <FormControl>
+                  <Input {...field} placeholder="My cool resume" autoFocus />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </div>
+  );
 };
 
 export default GeneralInfoFrom;
