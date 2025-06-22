04f3e3bdcba5855ab82017c3af2e0256243030e5
yash-251198 <shetyeyash444@gmail.com>
2025-06-23 02:51:27 +0530

    updated the title of metadata which will help the user to understand the page motive

--- src/app/(main)/billing/page.tsx
+++ src/app/(main)/billing/page.tsx
@@ -1,7 +1,12 @@
+import { Metadata } from "next";
 import React from "react";
 
+export const metadata: Metadata = {
+  title: "Subscription details",
+};
+
 const Page = () => {
-  return <main className="my-10 text-black">This is biiling page</main>;
+  return <main className="my-10">This is biiling page</main>;
 };
 
 export default Page;
--- src/app/(main)/resumes/page.tsx
+++ src/app/(main)/resumes/page.tsx
@@ -1,7 +1,11 @@
+import { Metadata } from "next";
 import React from "react";
 
+export const metadata: Metadata = {
+  title: "Resumes",
+};
+
 const Page = () => {
-  
   return <div>This is page where you can see your all resumes</div>;
 };
 
