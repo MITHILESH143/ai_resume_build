6c5d5d1d36641836a316a69ede1b3b18dd44c1b2
yash-251198 <shetyeyash444@gmail.com>
2025-06-23 02:56:41 +0530

    created the breadcrumb which will appear as link on ui to help navigate through the different form

---
+++ src/app/(main)/editor/BreadCrumbs.tsx
@@ -0,0 +1,47 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import React from "react";
+import { steps } from "./steps";
+
+interface BreadCrumbProps {
+  currentStep: string;
+  setCurrentStep: (step: string) => void;
+}
+
+const BreadCrumbs: React.FC<BreadCrumbProps> = ({
+  currentStep,
+  setCurrentStep,
+}) => {
+  return (
+    <div className="flex justify-center">
+      <Breadcrumb>
+        <BreadcrumbList>
+          {steps.map((step) => (
+            <React.Fragment key={step.key}>
+              <BreadcrumbItem>
+                {step.key == currentStep ? (
+                  <BreadcrumbPage>{step.title}</BreadcrumbPage>
+                ) : (
+                  <BreadcrumbLink asChild>
+                    <button onClick={() => setCurrentStep(step.key)}>
+                      {step.title}
+                    </button>
+                  </BreadcrumbLink>
+                )}
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="last:hidden"/>
+            </React.Fragment>
+          ))}
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
+  );
+};
+
+export default BreadCrumbs;
