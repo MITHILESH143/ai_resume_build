c78f561e55a4239c2629fa7523509a552f721e40
yash-251198 <shetyeyash444@gmail.com>
2025-06-25 01:16:20 +0530

    define a type for react.componentType as EditFormProps

--- src/app/(main)/editor/steps.ts
+++ src/app/(main)/editor/steps.ts
@@ -1,10 +1,11 @@
 import React from "react";
 import GeneralInfoFrom from "./forms/GeneralInfoFrom";
 import PersonalnfoForm from "./forms/PersonalnfoForm";
+import { EditorFormProps } from "@/lib/types";
 
 export const steps: {
   title: string;
-  component: React.ComponentType;
+  component: React.ComponentType<EditorFormProps>;
   key: string;
 }[] = [
   { title: "General Info", component: GeneralInfoFrom, key: "general-info" },
