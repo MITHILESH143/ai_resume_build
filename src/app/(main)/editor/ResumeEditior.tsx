dfb7f6c38640ee2ce7d1e8f5c4f7e25b7f3126ca
yash-251198 <shetyeyash444@gmail.com>
2025-07-06 21:30:53 +0530

    UPDATE: used useUnloadWarning , useAutoSave and useDebounce hooks in resumeeditor to create a whole autosaving action on resume data

--- src/app/(main)/editor/ResumeEditior.tsx
+++ src/app/(main)/editor/ResumeEditior.tsx
@@ -7,26 +7,30 @@ import { useState } from "react";
 import { ResumeValues } from "@/lib/validation";
 import ResumePreviewSections from "./ResumePreviewSections";
 import { cn } from "@/lib/utils";
+import useAutoSave from "./useAutoSave";
+import useUnloadWarning from "@/hooks/useUnloadWarning";
 
 const ResumeEditior = () => {
   const searchParams = useSearchParams();
   const currentStep = searchParams.get("step") || steps[0].key;
   const [showPreview, setShowPreview] = useState(false);
   const [resumeData, setResumeData] = useState<ResumeValues>({});
-  
+  const { isSaving, hasUnsavedChanges } = useAutoSave(resumeData);
+  useUnloadWarning(hasUnsavedChanges);
+
   function setStep(key: string) {
     const newSearchParams = new URLSearchParams(searchParams);
     newSearchParams.set("step", key);
     window.history.pushState(null, "", `?${newSearchParams.toString()}`);
   }
-  
+
   const FormComponent = steps.find(
     (step) => step.key === currentStep,
   )?.component;
-  
+
   return (
     <div className="flex h-screen flex-col">
-      <header className="space-y-1.5 border-b px-3 py-5 text-center flex-shrink-0">
+      <header className="flex-shrink-0 space-y-1.5 border-b px-3 py-5 text-center">
         <h1 className="text-2xl font-bold">Design Your Resume</h1>
         <p className="text-muted-foreground text-sm">
           Follow the steps below to create your resume. Your progress will be
@@ -34,14 +38,14 @@ const ResumeEditior = () => {
         </p>
       </header>
       <main className="flex flex-1 overflow-hidden">
-        <div className="flex w-full h-full">
+        <div className="flex h-full w-full">
           <div
             className={cn(
-              "flex w-full flex-col md:w-1/2 h-full",
+              "flex h-full w-full flex-col md:w-1/2",
               showPreview && "hidden",
             )}
           >
-            <div className="bg-background flex-shrink-0 z-10">
+            <div className="bg-background z-10 flex-shrink-0">
               <BreadCrumbs currentStep={currentStep} setCurrentStep={setStep} />
             </div>
             <div className="flex-1 overflow-y-auto p-4">
@@ -62,6 +66,7 @@ const ResumeEditior = () => {
         </div>
       </main>
       <Footer
+        isSaving={isSaving}
         currentStep={currentStep}
         setCurrentSteps={setStep}
         setShowPreview={setShowPreview}
