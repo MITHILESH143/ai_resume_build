0c33889c86b0cba7d142dc9629364f1122de4a65
yash-251198 <shetyeyash444@gmail.com>
2025-07-06 21:29:24 +0530

    NEW: created a new hook as autoSave to trigger the autosaving the data based on specified user action to prevent the loss of data even in page refresh

---
+++ src/app/(main)/editor/useAutoSave.tsx
@@ -0,0 +1,103 @@
+import useDebounce from "@/hooks/useDebounce";
+import { ResumeValues } from "@/lib/validation";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { saveResume } from "./actions";
+import { toast } from "sonner";
+import { Button } from "@/components/ui/button";
+
+const useAutoSave = (resumeData: ResumeValues) => {
+  const searchParams = useSearchParams();
+
+  const debounceData = useDebounce(resumeData, 1500);
+
+  const [resumeId, setResumeId] = useState(resumeData.id);
+
+  const [lastSavedData, setLastSavedData] = useState(
+    structuredClone(resumeData),
+  );
+
+  const [isSaving, setIsSaving] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    setIsError(false);
+  }, [debounceData]);
+
+  useEffect(() => {
+    const save = async () => {
+      try {
+        setIsSaving(true);
+        setIsError(false);
+        const newData = structuredClone(debounceData);
+        const updatedResume = await saveResume({
+          ...newData,
+          ...(lastSavedData.photo?.toString() === newData.photo?.toString() && {
+            photo: undefined,
+          }),
+          id: resumeId,
+        });
+
+        setResumeId(updatedResume.id);
+        setLastSavedData(newData);
+
+        if (searchParams.get("resumeId") !== updatedResume.id) {
+          const newSearchParams = new URLSearchParams(searchParams);
+          newSearchParams.set("resumeId", updatedResume.id);
+          window.history.replaceState(
+            null,
+            "",
+            `?${newSearchParams.toString()}`,
+          );
+        }
+      } catch (error) {
+        setIsError(true);
+        console.log(error);
+        toast(() => {
+          return (
+            <>
+              <div className="space-y-3">
+                <p>Could not save changes.</p>
+                <Button
+                  variant="secondary"
+                  onClick={() => {
+                    toast.dismiss();
+                    save();
+                  }}
+                >
+                  Retry
+                </Button>
+              </div>
+            </>
+          );
+        });
+      } finally {
+        setIsSaving(false);
+      }
+    };
+
+    const hasUnsavedChange =
+      JSON.stringify(resumeData) !== JSON.stringify(lastSavedData);
+
+    if (hasUnsavedChange && debounceData && !isSaving && !isError) {
+      save();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [
+    debounceData,
+    lastSavedData,
+    isSaving,
+    isError,
+    searchParams,
+    resumeId,
+    toast,
+  ]);
+
+  return {
+    isSaving,
+    hasUnsavedChanges:
+      JSON.stringify(resumeData) !== JSON.stringify(lastSavedData),
+  };
+};
+
+export default useAutoSave;
