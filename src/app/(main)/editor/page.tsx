a42034854db092fee5708250f46eb6c9e5ed4ac9
yash-251198 <shetyeyash444@gmail.com>
2025-07-08 00:33:40 +0530

    UPDATE: added a current editing resume data fetching mechanism using useeffect to prevent the loss of data on page refresh

--- src/app/(main)/editor/page.tsx
+++ src/app/(main)/editor/page.tsx
@@ -1,12 +1,33 @@
 import { Metadata } from "next";
 import ResumeEditior from "./ResumeEditior";
+import { auth } from "@clerk/nextjs/server";
+import { prisma } from "@/lib/prisma";
+import { resumeDataInclude } from "@/lib/types";
+
+interface PageProps {
+  searchParams: Promise<{ resumeId?: string }>;
+}
 
 export const metadata: Metadata = {
   title: "Design your resume",
 };
 
-const Page = () => {
-  return <ResumeEditior />;
+const Page = async ({ searchParams }: PageProps) => {
+  const { resumeId } = await searchParams;
+  const { userId } = await auth();
+
+  if (!userId) return;
+
+  const resumeToEdit = resumeId
+    ? await prisma.resume.findUnique({
+        where: { id: resumeId, userId },
+        include: resumeDataInclude,
+      })
+    : null;
+
+  console.log(resumeToEdit);
+
+  return <ResumeEditior resumeToEdit={resumeToEdit} />;
 };
 
 export default Page;
