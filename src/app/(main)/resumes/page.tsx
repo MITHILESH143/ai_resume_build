0dd13672e11768554706d268fe77103129c6b2bf
yash-251198 <shetyeyash444@gmail.com>
2025-07-13 04:07:40 +0530

    UDPATE:implemented a dynamic button which will help to create new resume and also open subsctiption model dialog when feature is not available.

--- src/app/(main)/resumes/page.tsx
+++ src/app/(main)/resumes/page.tsx
@@ -1,11 +1,8 @@
-import { Button } from "@/components/ui/button";
 import { prisma } from "@/lib/prisma";
 import { resumeDataInclude } from "@/lib/types";
 import { auth } from "@clerk/nextjs/server";
-import { PlusSquare } from "lucide-react";
 import { Metadata } from "next";
-import Link from "next/link";
-import React from "react";
+import CreateResumeButton from "./CreateResumeButton";
 import ResumeItem from "./ResumeItem";
 
 export const metadata: Metadata = {
@@ -37,13 +34,8 @@ const Page = async () => {
 
   return (
     <main className="mx-auto w-full max-w-7xl space-y-0 px-3 py-6">
-      <Button asChild className="mx-auto flex w-fit gap-2">
-        <Link href="/editor">
-          <PlusSquare className="size-5" />
-          New Resume
-        </Link>
-      </Button>
-      <div className="space-y-1 my-2">
+      <CreateResumeButton canCreate={totalCount < 3} />
+      <div className="my-2 space-y-1">
         <h1 className="text-3xl font-bold">Your Resumes</h1>
         <p>Total:{totalCount}</p>
       </div>
