c187945bdf3d1c224c8ffed35b38a37c1fa915a9
yash-251198 <shetyeyash444@gmail.com>
2025-07-13 04:06:25 +0530

    UPDATE:added a subscription dialog on the top of the app

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./Navbar";
+import PremiumModel from "@/components/premium/PremiumModel";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <Navbar />
       {children}
+      <PremiumModel />
     </div>
   );
 };
