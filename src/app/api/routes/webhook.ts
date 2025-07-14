11d87b2c9f1eebdcb24795b3c9cef87de6fa88cb
yash-251198 <shetyeyash444@gmail.com>
2025-07-15 02:29:28 +0530

    created a end point for webhooks

---
+++ src/app/api/routes/webhook.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+import crypto from "crypto";
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.text();
+    const signature = req.headers.get("x-razorpay-signature");
+
+    const expectedSignature = crypto
+      .createHmac("sha256", process.env.RAZORPAY_WEB_HOOK_SECRET!)
+      .update(body)
+      .digest("hex");
+
+    if (signature !== expectedSignature) {
+      return NextResponse.json({ error: "Invalid signature" }, { status: 401 });
+    }
+
+    const event = JSON.parse(body);
+  } catch (error) {
+    console.log(error);
+    throw new Error("Internal Server Error");
+  }
+}
