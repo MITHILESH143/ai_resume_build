c4f9f1ae207343774715dc83625f8607412ab1a0
yash-251198 <shetyeyash444@gmail.com>
2025-07-16 02:11:17 +0530

    solve the bug for not accepting request from forwarded port url

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
     serverActions: {
+      allowedOrigins: ["localhost:3000", "xjvn480g-3000.inc1.devtunnels.ms"],
       bodySizeLimit: "4mb",
     },
   },
@@ -11,7 +12,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname:"6vqprsktfd3qqvtb.public.blob.vercel-storage.com"
+        hostname: "6vqprsktfd3qqvtb.public.blob.vercel-storage.com",
       },
     ],
   },
